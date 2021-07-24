f7534948e99e6f14910a34e13150e321631119ed
Simon Redman <simon@ergotech.com>
2021-07-24 14:39:58 -0700

    Add test enforcing class hierarchy of coordinates

--- tests/tests.py
+++ tests/tests.py
@@ -10,6 +10,7 @@ class TestCartesian2D(unittest.TestCase):
         val = cartesian2D.Cartesian2D(1, 2)
         self.assertEqual(val.x, 1)
         self.assertEqual(val.y, 2)
+        self.assertTrue(issubclass(type(val), coordinate.Coordinate), "All coordinate types should be subclasses of Coordinate")
 
 class TestCartesian3D(unittest.TestCase):
 
@@ -18,6 +19,7 @@ class TestCartesian3D(unittest.TestCase):
         self.assertEqual(val.x, 1)
         self.assertEqual(val.y, 2)
         self.assertEqual(val.z, 3)
+        self.assertTrue(issubclass(type(val), coordinate.Coordinate), "All coordinate types should be subclasses of Coordinate")
 
 class TestPolar(unittest.TestCase):
 
@@ -25,6 +27,7 @@ class TestPolar(unittest.TestCase):
         val = polar.Polar(1, 45)
         self.assertEqual(val.r, 1)
         self.assertEqual(val.theta, 45)
+        self.assertTrue(issubclass(type(val), coordinate.Coordinate), "All coordinate types should be subclasses of Coordinate")
 
 class TestSpherical(unittest.TestCase):
 
@@ -33,6 +36,7 @@ class TestSpherical(unittest.TestCase):
         self.assertEqual(val.rho, 1)
         self.assertEqual(val.theta, 45)
         self.assertEqual(val.phi, 45)
+        self.assertTrue(issubclass(type(val), coordinate.Coordinate), "All coordinate types should be subclasses of Coordinate")
 
 if __name__ == '__main__':
     unittest.main()
