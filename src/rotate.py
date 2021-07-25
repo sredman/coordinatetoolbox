d92932c84027163f274620d6fac9ef6e706a02c7
Simon Redman <simon@ergotech.com>
2021-07-24 18:01:48 -0700

    Implement rotation

--- src/rotate.py
+++ src/rotate.py
@@ -3,45 +3,51 @@
 # @filename: rotate.py
 # @description: Rotates coordinates about given vectors
 
+import math
 import numpy as np
 import scipy as sp
 from scipy.spatial.transform import Rotation as R
 
 from typing import Tuple
 
+from .convert import Convert
+from .coordinates.coordinate import Coordinate
+from .coordinates.spherical import Spherical
+from .coordinates.cartesian3D import Cartesian3D
+
 class Rotate:
 
-  def __init__(self, coordinate: coordinate.Coordinate):
+  def __init__(self, coordinate: Coordinate):
     self.coordinate = coordinate
 
-  def aboutXAxis(self, rotation: float) -> coordinate.Coordinate:
+  def aboutXAxis(self, rotation: float) -> Coordinate:
     """
     Rotate the input coordinate a given number of degrees around the X axis
 
     rotation: float
       The angle (in degrees) to rotate
     """
-    pass
+    return self.aboutVector(aboutvec=[1, 0, 0], rotation=rotation)
 
-  def aboutYAxis(self, rotation: float) -> coordinate.Coordinate:
+  def aboutYAxis(self, rotation: float) -> Coordinate:
     """
     Rotate the input coordinate a given number of degrees around the Y axis
 
     rotation: float
       The angle (in degrees) to rotate
     """
-    pass
+    return self.aboutVector(aboutvec=[0, 1, 0], rotation=rotation)
 
-  def aboutZAxis(self, rotation: float) -> coordinate.Coordinate:
+  def aboutZAxis(self, rotation: float) -> Coordinate:
     """
     Rotate the input coordinate a given number of degrees around the Z axis
 
     rotation: float
       The angle (in degrees) to rotate
     """
-    pass
+    return self.aboutVector(aboutvec=[0, 0, 1], rotation=rotation)
 
-  def aboutVector(self, aboutvec: Tuple[float, float, float], rotation: float) -> coordinate.Coordinate:
+  def aboutVector(self, aboutvec: Tuple[float, float, float], rotation: float) -> Coordinate:
     """
     Rotate the input coordinate a given number of degrees around the given vector
 
@@ -50,4 +56,14 @@ class Rotate:
     rotation: float
       The angle (in degrees) to rotate
     """
-    pass
+    rotvec = R.from_rotvec(math.radians(rotation) * np.array(aboutvec))
+    input = Convert(self.coordinate).toCartesian3D()
+    outvec = rotvec.apply(input)
+    retval = Cartesian3D(outvec[0], outvec[1], outvec[2])
+
+    # Now convert that output back to what the user actually wanted to rotate
+    if isinstance(self.coordinate, Spherical):
+      return Convert(retval).toSpherical();
+    if isinstance(self.coordinate, Cartesian3D):
+      return Convert(retval).toCartesian3D();
+    raise NotImplementedError("Not prepared to rotate a {type}".format(self.coordinate.__class__.__name__));
---
+++ tests/test_Rotate.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+# @author: Simon Redman <simon@ergotech.com>
+# @date: 2021-07-24
+# @filename: TestCartesian3D.py
+# @description: Test cases for the Cartesian3D coordinate type
+
+import unittest
+import math
+
+from src.coordinates.coordinate import Coordinate
+from src.coordinates.cartesian2D import Cartesian2D
+from src.coordinates.cartesian3D import Cartesian3D
+from src.coordinates.polar import Polar
+from src.coordinates.spherical import Spherical
+
+from src.rotate import Rotate
+
+class TestRotate(unittest.TestCase):
+  """
+  Test cases for rotations
+  """
+
+  def test_rotate_xunit_xaxis(self):
+    """
+    Test that a rotation of the x unit-vector about the x axis returns correctly
+    """
+    val = Cartesian3D(1, 0, 0)
+    expected = Cartesian3D(1, 0, 0)
+    result = Rotate(val).aboutXAxis(90)
+    self.fuzzyCompareCartesian3D(expected, result)
+
+  def test_rotate_xunit_yaxis(self):
+    """
+    Test that a rotation of the x unit-vector about the y axis returns correctly
+    """
+    val = Cartesian3D(1, 0, 0)
+    expected = Cartesian3D(0, 0, -1)
+    result = Rotate(val).aboutYAxis(90)
+    self.fuzzyCompareCartesian3D(expected, result)
+
+  def test_rotate_xunit_zaxis(self):
+    """
+    Test that a rotation of the x unit-vector about the z axis returns correctly
+    """
+    val = Cartesian3D(1, 0, 0)
+    expected = Cartesian3D(0, 1, 0)
+    result = Rotate(val).aboutZAxis(90)
+    self.fuzzyCompareCartesian3D(expected, result)
+
+  def test_rotate_yunit_xaxis(self):
+    """
+    Test that a rotation of the y unit-vector about the x axis returns correctly
+    """
+    val = Cartesian3D(0, 1, 0)
+    expected = Cartesian3D(0, 0, 1)
+    result = Rotate(val).aboutXAxis(90)
+    self.fuzzyCompareCartesian3D(expected, result)
+
+  def test_rotate_neg_yunit_xaxis(self):
+    """
+    Test that a rotation of the negative y unit-vector about the x axis returns correctly
+    """
+    val = Cartesian3D(0, -1, 0)
+    expected = Cartesian3D(0, 0, -1)
+    result = Rotate(val).aboutXAxis(90)
+    self.fuzzyCompareCartesian3D(expected, result)
+
+  def test_rotate_yunit_yaxis(self):
+    """
+    Test that a rotation of the y unit-vector about the y axis returns correctly
+    """
+    val = Cartesian3D(0, 1, 0)
+    expected = Cartesian3D(0, 1, 0)
+    result = Rotate(val).aboutYAxis(90)
+    self.fuzzyCompareCartesian3D(expected, result)
+
+  def test_rotate_yunit_zaxis(self):
+    """
+    Test that a rotation of the y unit-vector about the z axis returns correctly
+    """
+    val = Cartesian3D(0, 1, 0)
+    expected = Cartesian3D(-1, 0, 0)
+    result = Rotate(val).aboutZAxis(90)
+    self.fuzzyCompareCartesian3D(expected, result)
+
+  def test_rotate_zunit_xaxis(self):
+    """
+    Test that a rotation of the z unit-vector about the x axis returns correctly
+    """
+    val = Cartesian3D(0, 0, 1)
+    expected = Cartesian3D(0, -1, 0)
+    result = Rotate(val).aboutXAxis(90)
+    self.fuzzyCompareCartesian3D(expected, result)
+
+  def test_rotate_zunit_yaxis(self):
+    """
+    Test that a rotation of the z unit-vector about the y axis returns correctly
+    """
+    val = Cartesian3D(0, 0, 1)
+    expected = Cartesian3D(1, 0, 0)
+    result = Rotate(val).aboutYAxis(90)
+    self.fuzzyCompareCartesian3D(expected, result)
+
+  def test_rotate_zunit_zaxis(self):
+    """
+    Test that a rotation of the z unit-vector about the z axis returns correctly
+    """
+    val = Cartesian3D(0, 0, 1)
+    expected = Cartesian3D(0, 0, 1)
+    result = Rotate(val).aboutZAxis(90)
+    self.fuzzyCompareCartesian3D(expected, result)
+
+  def test_rotate_idvec_xaxis(self):
+    """
+    Test that a rotation of the identity vector about the x axis returns correctly
+    """
+    val = Cartesian3D(1, 1, 1)
+    expected = Cartesian3D(1, -1, 1)
+    result = Rotate(val).aboutXAxis(90)
+    self.fuzzyCompareCartesian3D(expected, result)
+
+  def test_rotate_idvec_yaxis(self):
+    """
+    Test that a rotation of the identity vector about the y axis returns correctly
+    """
+    val = Cartesian3D(1, 1, 1)
+    expected = Cartesian3D(1, 1, -1)
+    result = Rotate(val).aboutYAxis(90)
+    self.fuzzyCompareCartesian3D(expected, result)
+
+  def test_rotate_idvec_zaxis(self):
+    """
+    Test that a rotation of the identity vector about the z axis returns correctly
+    """
+    val = Cartesian3D(1, 1, 1)
+    expected = Cartesian3D(-1, 1, 1)
+    result = Rotate(val).aboutZAxis(90)
+    self.fuzzyCompareCartesian3D(expected, result)
+
+  def fuzzyCompareCartesian3D(self, val1: Cartesian3D, val2: Cartesian3D, places=3):
+    self.assertAlmostEqual(val1.x, val2.x, places=places)
+    self.assertAlmostEqual(val1.y, val2.y, places=places)
+    self.assertAlmostEqual(val1.z, val2.z, places=places)
