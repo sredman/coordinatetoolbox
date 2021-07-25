fc3638e89830b04bad306ee71aef93318b70cc7a
Simon Redman <simon@ergotech.com>
2021-07-24 23:15:54 -0700

    Remove unnecessary else

--- coordinatetoolbox.py
+++ coordinatetoolbox.py
@@ -16,9 +16,9 @@ from src.rotate import Rotate
 from src.convert import Convert
 
 class CustomHelpFormatter(HelpFormatter):
-    """
-    Help formatter for Coordinate Toolbox
-    """
+  """
+  Help formatter for Coordinate Toolbox
+  """
 
   def _format_action_invocation(self, action):
     if not action.option_strings:
@@ -26,21 +26,18 @@ class CustomHelpFormatter(HelpFormatter):
       default = self._get_default_metavar_for_positional(action)
       metavar, = self._metavar_formatter(action, default)(1)
       return metavar
-
+    parts = []
+    if action.nargs == 0:
+      # Just add options, if they expects no values (like --help)
+      parts.extend(action.option_strings)
     else:
-      parts = []
-      if action.nargs == 0:
-        # Just add options, if they expects no values (like --help)
-        parts.extend(action.option_strings)
-      else:
-        default = self._get_default_metavar_for_optional(action)
-        args_string = self._format_args(action, default)
-        for option_string in action.option_strings:
-          parts.append(option_string)
-        # Join the argument names (like -p --param ) and add the metavar at the end
-        return '%s %s' % (', '.join(parts), args_string)
-
-      return ', '.join(parts)
+      default = self._get_default_metavar_for_optional(action)
+      args_string = self._format_args(action, default)
+      for option_string in action.option_strings:
+        parts.append(option_string)
+      # Join the argument names (like -p --param ) and add the metavar at the end
+      return '%s %s' % (', '.join(parts), args_string)
+    return ', '.join(parts)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(
