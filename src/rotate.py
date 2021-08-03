6c7b8034725a70bf7597a6e814129ddec13809d0
Simon Redman <simon@ergotech.com>
2021-08-02 22:34:34 -0700

    Rename `input` to `invec`

--- src/rotate.py
+++ src/rotate.py
@@ -61,8 +61,8 @@ class Rotate:
       The angle (in degrees) to rotate
     """
     rotvec = R.from_rotvec(math.radians(rotation) * np.array(aboutvec))
-    input = Convert(self.coordinate).toCartesian3D()
-    outvec = rotvec.apply(input)
+    invec = Convert(self.coordinate).toCartesian3D()
+    outvec = rotvec.apply(invec)
     retval = Cartesian3D(outvec[0], outvec[1], outvec[2])
 
     # Now convert that output back to what the user actually wanted to rotate
